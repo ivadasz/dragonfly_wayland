db8d8ef5f0985e9c1f6d4b39a905117145729382
Imre Vadász <imre@vdsz.com>
2015-10-06 22:50:17 +0200

    Properly imitate mremap behaviour utilizing MAP_TRYFIXED, needs storing the fd in the wl_shm_pool of the server.

--- files/patch-cursor_wayland-cursor.c
+++ files/patch-cursor_wayland-cursor.c
@@ -1,35 +1,14 @@
 --- cursor/wayland-cursor.c.orig	2015-06-13 00:51:38 +0200
 +++ cursor/wayland-cursor.c
-@@ -56,6 +56,13 @@
- 	if (!pool)
- 		return NULL;
+@@ -97,7 +97,11 @@
  
-+	/*
-+	 * This makes sure that enough space is available to avoid resizing,
-+	 * of the pool used for cursors.
-+	 */
-+	if (size < 0x8000)
-+		size = 0x8000;
-+
- 	pool->fd = os_create_anonymous_file(size);
- 	if (pool->fd < 0)
- 		goto err_free;
-@@ -91,6 +98,18 @@
- 		return 0;
- #endif
- 
-+#if 0
+ 	pool->data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED,
+ 			  pool->fd, 0);
 +#if defined(__DragonFly__)
-+	void *data;
-+        data = mmap((void *)pool->data + pool->used, size - pool->size,
-+                    PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, pool->fd,
-+                    pool->used);
-+	if (data == (void *)-1) {
-+		fprintf(stderr, "failed with MAP_FIXED\n");
-+		return 0;
-+	}
++	if (pool->data == MAP_FAILED)
++#else
+ 	if (pool->data == (void *)-1)
 +#endif
-+#endif
- 	wl_shm_pool_resize(pool->pool, size);
+ 		return 0;
+ 	pool->size = size;
  
- 	munmap(pool->data, pool->size);
--- files/patch-src_wayland-shm.c
+++ files/patch-src_wayland-shm.c
@@ -1,15 +1,37 @@
 --- src/wayland-shm.c.orig	2015-06-13 00:31:24 +0200
 +++ src/wayland-shm.c
-@@ -140,6 +140,8 @@
- 		return;
- 	}
+@@ -30,6 +30,10 @@
  
-+//	wl_log("%s: offset=%d, width=%d, height=%d, stride=%d, pool->size=%d\n",
-+//	    __func__, offset, width, height, stride, pool->size);
- 	if (offset < 0 || width <= 0 || height <= 0 || stride < width ||
- 	    INT32_MAX / stride <= height ||
- 	    offset > pool->size - stride * height) {
-@@ -205,8 +207,16 @@
+ #define _GNU_SOURCE
+ 
++#if defined(__DragonFly__)
++#include <sys/param.h>
++#endif
++
+ #include <stdbool.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+@@ -55,6 +59,9 @@
+ 	int refcount;
+ 	char *data;
+ 	int32_t size;
++#if defined(__DragonFly__)
++	int fd;
++#endif
+ };
+ 
+ struct wl_shm_buffer {
+@@ -80,6 +87,9 @@
+ 		return;
+ 
+ 	munmap(pool->data, pool->size);
++#if defined(__DragonFly__)
++	close(pool->fd);
++#endif
+ 	free(pool);
+ }
+ 
+@@ -205,8 +215,27 @@
  				       "shrinking pool invalid");
  		return;
  	}
@@ -17,35 +39,64 @@
 +		return;
  
 +#if defined(__DragonFly__)
-+	wl_log("%s: mremap from %d to %d attempted\n", __func__,
-+	    pool->size, size);
-+	data = MAP_FAILED;
++	int32_t osize = (pool->size + PAGE_SIZE - 1) & ~PAGE_MASK;
++	if (size <= osize) {
++		pool->size = size;
++		return;
++	}
++	data = mmap(pool->data + osize, size - osize, PROT_READ,
++	    MAP_SHARED | MAP_TRYFIXED, pool->fd, osize);
++	if (data == MAP_FAILED) {
++		munmap(pool->data, pool->size);
++		data = mmap(NULL, size, PROT_READ, MAP_SHARED, pool->fd, 0);
++	} else {
++		pool->size = size;
++		return;
++	}
 +#else
  	data = mremap(pool->data, pool->size, size, MREMAP_MAYMOVE);
 +#endif
  	if (data == MAP_FAILED) {
  		wl_resource_post_error(resource,
  				       WL_SHM_ERROR_INVALID_FD,
-@@ -243,6 +253,10 @@
- 		goto err_free;
- 	}
+@@ -245,21 +274,27 @@
  
-+	/* Makes sure that cursor handling works without needing mremap */
-+	if (size < 0x8000)
-+		size = 0x8000;
-+
  	pool->refcount = 1;
  	pool->size = size;
- 	pool->data = mmap(NULL, size,
-@@ -449,6 +463,15 @@
+-	pool->data = mmap(NULL, size,
+-			  PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
++	pool->data = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+ 	if (pool->data == MAP_FAILED) {
+ 		wl_resource_post_error(resource,
+ 				       WL_SHM_ERROR_INVALID_FD,
+ 				       "failed mmap fd %d", fd);
+ 		goto err_close;
+ 	}
++#if defined(__DragonFly__)
++	pool->fd = fd;
++#else
+ 	close(fd);
++#endif
+ 
+ 	pool->resource =
+ 		wl_resource_create(client, &wl_shm_pool_interface, 1, id);
+ 	if (!pool->resource) {
+ 		wl_client_post_no_memory(client);
+ 		munmap(pool->data, pool->size);
++#if defined(__DragonFly__)
++		close(fd);
++#endif
+ 		free(pool);
+ 		return;
+ 	}
+@@ -449,6 +484,14 @@
  	sigbus_data->fallback_mapping_used = 1;
  
  	/* This should replace the previous mapping */
 +#if defined(__DragonFly__)
 +	if (mmap(pool->data, pool->size,
-+		 PROT_READ | PROT_WRITE,
-+		 MAP_PRIVATE | MAP_FIXED | MAP_ANON,
-+		 0, 0) == (void *) -1) {
++		 PROT_READ, MAP_PRIVATE | MAP_FIXED | MAP_ANON,
++		 0, 0) == MAP_FAILED) {
 +		reraise_sigbus();
 +		return;
 +	}
@@ -53,7 +104,7 @@
  	if (mmap(pool->data, pool->size,
  		 PROT_READ | PROT_WRITE,
  		 MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS,
-@@ -456,6 +479,7 @@
+@@ -456,6 +499,7 @@
  		reraise_sigbus();
  		return;
  	}
